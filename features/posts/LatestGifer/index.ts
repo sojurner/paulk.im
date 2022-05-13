b035393e3ee5d8c91319fe8e73e9e7f40b9e444c
Paul Kim <pykim0591@gmail.com>
2022-05-13 09:27:22 -0600

    Remove Gifer

--- features/posts/LatestGifer/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './LatestGifer'
-export * as Types from './types'
--- features/posts/LatestGifer/types.ts
+++
@@ -1,7 +0,0 @@
-import { ReactJSXIntrinsicAttributes } from '@emotion/react/types/jsx-namespace';
-import { DetailedHTMLProps, IframeHTMLAttributes } from 'react';
-
-export type Props = DetailedHTMLProps<
-  IframeHTMLAttributes<HTMLIFrameElement>,
-  HTMLIFrameElement
->;
