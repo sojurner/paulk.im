49374326ad85cc1613456fb7e1b86f69c07c9f7b
Paul Kim <pykim0591@gmail.com>
2022-05-13 15:20:04 -0600

    Remove footer

--- features/about/AboutRoot.tsx
+++ features/about/AboutRoot.tsx
@@ -1,8 +1,6 @@
-import NextLink from 'next/link';
 import { Flex, useColorModeValue } from '@chakra-ui/react';
 
 import { RegularText, SubTitle } from '@/components/Typography';
-import { Footerbar } from '@/components/Footerbar';
 
 import { RandomFacts } from './RandomFacts';
 import { Skillset } from './Skillset';
@@ -10,7 +8,7 @@ import { Resources } from './Resources';
 import { Following } from './Following';
 
 export const AboutRoot = () => {
-  const h2Color =  useColorModeValue('pink.400', 'pink.300')
+  const h2Color = useColorModeValue('pink.400', 'pink.300');
   return (
     <Flex
       overflow="auto"
@@ -29,9 +27,11 @@ export const AboutRoot = () => {
       }}
     >
       <Flex flexDirection="column" width={['95%', '560px']}>
-        <SubTitle color={h2Color} my="4">About Me...</SubTitle>
+        <SubTitle color={h2Color} my="4">
+          About Me...
+        </SubTitle>
         <RegularText fontSize={'1.2em'}>
-          Hello, My name is Paul.  Im a full-stack developer based in Colorado.
+          Hello, My name is Paul. Im a full-stack developer based in Colorado.
         </RegularText>
         <RegularText mt="3" fontSize={'1.2em'}>
           Here is my current list of tech know-hows:
@@ -53,7 +53,6 @@ export const AboutRoot = () => {
           Currently Following...
         </SubTitle>
         <Following />
-        <Footerbar pos="fixed" width="100vw" left="0" height="30px" bottom={0} />
       </Flex>
     </Flex>
   );
