8407c055fbce10578efeff0dd32d55da4291dcad
Paul Kim <pykim0591@gmail.com>
2022-08-08 06:55:48 -0600

    Add desc

--- features/about/Explained.tsx
+++ features/about/Explained.tsx
@@ -1,6 +1,6 @@
 import NextLink from 'next/link';
 
-import { IdeaIcon, Logo, Pepe } from '@/components/Icon';
+import { IdeaIcon, Logo, MemeIcon, Pepe } from '@/components/Icon';
 import { RegularText } from '@/components/Typography';
 import { Box, Flex, VStack, Link, Text } from '@chakra-ui/react';
 import { usePlaylistContext } from '../playlist';
@@ -24,7 +24,7 @@ export const PepeLink = () => {
 
   return (
     <RegularText fontSize={'1.2em'}>
-      Pepe is many things. But most importantly:{' '}
+      This is Pepe the frog. Pepe is many things. But most importantly:{' '}
       <Text
         as="ins"
         color="green.400"
@@ -42,78 +42,69 @@ export const Explained = () => {
   return (
     <VStack spacing="5" alignItems="flex-start">
       <RegularText fontSize="1.2em">
-        This website is a 4 year culmination of trial & errors. I suppose in
-        another 4 years, it will look completely different.
-      </RegularText>
-      <RegularText fontSize="1.2em">
-        I went with short form content, because it felt society&apos;s media
-        consumption habits was heading towards this direction. I&apos;ll try my
-        best to keep it funny, wholesome, and relatively kosher.
+        This website is a four year culmination of trial & errors. I suppose in
+        another couple years, it will look completely different.
       </RegularText>
       <Flex>
         <NextLink href="/">
           <div>
-            <Box cursor="pointer" mr="6" fontSize="4em">
+            <Box cursor="pointer" mr="6" fontSize="3.5em">
               <Logo />
             </Box>
           </div>
         </NextLink>
         <VStack alignItems="flex-start">
           <RegularText fontSize={'1.2em'}>
-            I hi-jacked this Logo from this{' '}
+            I hi-jacked this Logo from {' '}
             <Link
               href="https://www.shutterstock.com/image-vector/modern-abstract-letter-pk-logo-this-1516950476"
               isExternal
               textDecor="underline"
               color="blue.400"
             >
-              Link
+              Gfxvect
             </Link>
             . I just converted it into an svg, and filled it with random colors.
-            I&apos;m a developer, not a designer.
-          </RegularText>
-          <RegularText fontSize={'1.2em'}>
-            Here is where I share videos, songs, and images I find throughout
-            the week.
+            I&apos;ve come to accept my limitations when it comes design.
           </RegularText>
         </VStack>
       </Flex>
       <Flex>
         <NextLink href="/tils">
           <div>
-            <Box cursor="pointer" mr="6" fontSize="4em">
+            <Box cursor="pointer" mr="6" fontSize="3.5em">
+              <MemeIcon />
+            </Box>
+          </div>
+        </NextLink>
+        <RegularText fontSize={'1.2em'}>
+          Here is where I share funny and interesting videos, songs, and images
+          I discover throughout the week.
+        </RegularText>
+      </Flex>
+      <Flex>
+        <NextLink href="/tils">
+          <div>
+            <Box cursor="pointer" mr="6" fontSize="3.5em">
               <IdeaIcon />
             </Box>
           </div>
         </NextLink>
         <VStack alignItems="flex-start">
           <RegularText fontSize={'1.2em'}>
-            I probably wasted a good 2 hours looking for a{' '}
-            <strong>Today I Learned (TIL)</strong> icon. Still looking... but
-            for now, I&apos;ve settled on this one.
-          </RegularText>
-          <RegularText fontSize={'1.2em'}>
-            Here is where I&apos;ll post code snippets and random educational
-            fun facts.
+            Here is where I post code snippets and random educational fun facts.  
           </RegularText>
         </VStack>
       </Flex>
       <Flex>
         <NextLink href="/about">
           <div>
-            <Box mr="6" fontSize="4em">
+            <Box mr="6" fontSize="3.5em">
               <Pepe />
             </Box>
           </div>
         </NextLink>
         <VStack alignItems="flex-start">
-          <RegularText fontSize={'1.2em'}>
-            Recently, a close friend of mine asked why I chose this
-            &quot;frog&quot; image. Then, it dawned on me
-            that he was unaware of the
-            <Text as="strong"> Pepe the Frog</Text> meme. I never thought
-            I&apos;d find a friend who lived under a bigger rock than I did.
-          </RegularText>
           <PepeLink />
         </VStack>
       </Flex>
--- features/about/Intro.tsx
+++ features/about/Intro.tsx
@@ -12,6 +12,10 @@ export const Intro = () => {
         Here is my current list of tech know-hows:
       </RegularText>
       <Skillset width={['95%', 540]} />
+
+      <RegularText mt="3" fontSize={'1.2em'}>
+        I enjoy sharing ideas and learning of stories from all walks of life!
+      </RegularText>
     </>
   );
 };
